b784c81fc889dad0a8d58475ecf08d16afe9b844
abn <abn.dev.01@gmail.com>
2021-07-22 11:31:19 +0300

    start the project

---
+++ src/main/java/demo/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package demo.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("Spring Boot Demo project")
+                            .description("Spring Boot demo application.")
+                            .version("v0.0.1")
+//                            .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+            )
+            .externalDocs(new ExternalDocumentation()
+                              .description("SpringShop Wiki Documentation")
+                              .url("https://springshop.wiki.github.org/docs"));
+    }
+}
