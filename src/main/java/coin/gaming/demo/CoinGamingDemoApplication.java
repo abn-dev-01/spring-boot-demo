c6910c60dbf0e753fb29d1d4101491ccce700e58
abn <abn.dev.01@gmail.com>
2021-07-22 11:31:19 +0300

    start the project

---
+++ src/main/java/coin/gaming/demo/CoinGamingDemoApplication.java
@@ -0,0 +1,12 @@
+package coin.gaming.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinGamingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinGamingDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/coin/gaming/demo/ActuatorHealthIT.java
@@ -0,0 +1,37 @@
+package coin.gaming.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ActuatorHealthIT {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnCorrectActuatorHealthValue() {
+        Map<String, String> response = restTemplate.exchange(
+            "http://localhost:" + port + "/actuator/health",
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<Map<String, String>>() {
+            }
+        ).getBody();
+
+        assertNotNull(response);
+        assertEquals("UP", response.get("status"));
+    }
+}
